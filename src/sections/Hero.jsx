740cb4493b3032ab4cc9094e1f65dc9d59be178e
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-04-16 02:07:37 +0800

    Add HeroExperience component and integrate it into Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { words } from "../constants";
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 
 const Hero = () => {
   return (
@@ -11,8 +12,8 @@ const Hero = () => {
 
       <div className="hero-layout">
         {/* LEFT: HERO CONTENT */}
-        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5 gap-7">
-          <div className="flex flex-col ">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5 ">
+          <div className="flex flex-col gap-7 ">
             <div className="hero-text">
               <h1>
                 Shaping
@@ -37,17 +38,22 @@ const Hero = () => {
               <h1>into Real Project</h1>
               <h1>that Deliver Results</h1>
             </div>
-          </div>
-
-          <p className="text-white-50 md:text-xl relative z-10 pointer-events-none ">
-            Hi, I&apos;m Gerald, a sophomore computer enginering student with a
-            passion for code.
-          </p>
-          <Button
-            text="See My Work"
-            className="md:w-80 md:h-16 w-60 h-5"
-            id="counter"
-          />
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none ">
+              Hi, I&apos;m Gerald, a sophomore computer enginering student with
+              a passion for code.
+            </p>
+            <Button
+              text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-5"
+              id="counter"
+            />
+          </div>{" "}
+          {/* RIGHT: 3D MODEL */}
+          <figure>
+            <div className="hero-3d-layout">
+              <HeroExperience />
+            </div>
+          </figure>
         </header>
       </div>
     </section>
