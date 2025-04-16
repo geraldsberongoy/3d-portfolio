e3b6f80393701565e3f4bb15604ba21102df2798
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-04-16 16:49:02 +0800

    Add Particles import and adjust minDistance in HeroExperience component

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -4,6 +4,7 @@ import { useMediaQuery } from "react-responsive";
 import React from "react";
 import { Room } from "./Room";
 import HeroLights from "./HeroLights";
+import Particles from "./Particles";
 
 
 const HeroExperience = () => {
@@ -13,15 +14,16 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <HeroLights/>
+      {/* <Particles count={isMobile ? 50 : 100} /> Adjust particle count based on device size */}
       <OrbitControls
         enablePan={false} // Prevents panning of the scene
         enableZoom={!isTablet} // Disables zoom on tablets
         maxDistance={20} // Maximum distance for zooming out
-        minDistance={5} // Minimum distance for zooming in
+        minDistance={10} // Minimum distance for zooming in
         minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
         maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
       />
-      <group  scale={isMobile ? 0.7 : 1} position={[0, -3.5,0 ]} rotation={[0, -Math.PI /4, 0]}>
+      <group  scale={isMobile ? 0.7 : 1} position={[0, -3.5, 0 ]} rotation={[0, -Math.PI /4, 0]}>
         <Room />
       </group>
     </Canvas>
